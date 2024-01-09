31c40d32f75fa14bd1a8ea548fafb239b49aab70
vinoku <vinoku78@gmail.com>
2024-01-10 00:39:14 +0100

    use jackson

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,8 +1,11 @@
 package com.pluralsight.courseinfo.cli;
 
 import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
+import com.pluralsight.courseinfo.cli.service.PluralsightCourse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class CourseRetriever {
 
@@ -22,11 +25,20 @@ public class CourseRetriever {
         }
     }
 
+    /**
+     *
+     * Testing with different author ids: sander-mak, jim-weaver, bryan-hansen, richard-warburton, jose-paumard
+     *
+     * @param authorId - Pluralsight ID of author
+     */
     private static void retrieveCourses(String authorId) {
-        LOG.info("Retrieving courses for author '{}'",authorId);
+        LOG.info("Retrieving courses for author '{}'", authorId);
         CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
 
-        String coursesToStore = courseRetrievalService.getCoursesFor(authorId);
-        LOG.info("Retrieved the following courses {}", coursesToStore);
+        List<PluralsightCourse> coursesToStore = courseRetrievalService.getCoursesFor(authorId)
+                .stream()
+                .filter(course -> !course.isRetired())
+                .toList();
+        LOG.info("Retrieved the following {} courses {}", coursesToStore.size(), coursesToStore);
     }
 }
