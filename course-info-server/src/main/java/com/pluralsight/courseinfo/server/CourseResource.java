5b0893ac1cb08d7f8097a1c2a38695b0819e7ebf
vinoku <vinoku78@gmail.com>
2024-01-10 14:24:36 +0100

    implement REST endpoint

---
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
@@ -0,0 +1,42 @@
+package com.pluralsight.courseinfo.server;
+
+import com.pluralsight.courseinfo.domain.Course;
+import com.pluralsight.courseinfo.repository.CourseRepository;
+import com.pluralsight.courseinfo.repository.RepositoryException;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Path("/courses")
+public class CourseResource {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseResource.class);
+
+    private final CourseRepository courseRepository;
+
+    public CourseResource(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Course> getCourses() {
+        try {
+            return courseRepository
+                    .getAllCourses()
+                    .stream()
+                    .sorted(Comparator.comparing(Course::id))
+                    .toList();
+        } catch (RepositoryException e) {
+            throw new NotFoundException();
+        }
+    }
+}
